3c9591cd5d948c185bdb6edc39bfdc38141fa729
AseelQabbaha <aseelqabbaha@gmail.com>
2025-08-19 18:25:08 +0300

    backend

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -6,4 +6,49 @@ function authorizeRole(role) {
   };
 }
 
+
+const allowedRoles=(req,res,next)=>{
+try{
+if(!req.token ||req.token.role){
+  return res.status(403).json({
+    success:false,
+    message:"Access failed"
+    
+  })
+}
+
+const userRole=req.token.role
+
+if(!allowedRoles.includes(userRole)){
+  return res.status(403).json({
+    success:false,
+    message:"insufficient permission"
+    
+  })
+}
+next()
+
+}catch(err){
+res.status(500).json({
+    success:false,
+    message:"Athorization Error",
+    error:err.message
+  })
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 module.exports={authorizeRole}
